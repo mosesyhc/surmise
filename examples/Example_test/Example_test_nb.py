bias covariance fix

examples/Example_test/Example_test_nb.py
@@ -196,8 +196,9 @@ def plot_pred(x_std, xrep, y, cal, theta_range):
         bias = cal.info['bias'].predict(theta=x_std)
         upperb = np.squeeze(bias.mean() + np.sqrt(1.96*bias.var()))
         lowerb = np.squeeze(bias.mean() - np.sqrt(1.96*bias.var()))
-        axs[3].plot(xrep[0:21].reshape(21), bias.mean().T, color='grey')
-        axs[3].fill_between(xrep[0:21].reshape(21), lowerb, upperb, color = 'grey', alpha=0.5)
+        axs[3].plot(xrep[0:21].reshape(21), bias.mean().T, color='grey', linestyle='--')
+        axs[3].fill_between(xrep[0:21].reshape(21), lowerb, upperb,
+                            color = 'grey', alpha=0.5)
         axs[3].plot(xrep[0:21].reshape(21), b(xrep[0:21]), color='green')
 
 
@@ -226,7 +227,7 @@ cal_2 = calibrator(emu=emulator_1,
                    y=y,
                    x=xrep_std,
                    thetaprior=prior_balldrop,
-                   method='directbayeswoodbury',
+                   method='directbayeswoodbury_discrep_mod',
                    yvar=obsvar)
 
 plot_pred(x_std, xrep, y, cal_2, theta_range)

surmise/calibrationmethods/directbayeswoodbury_discrep_mod.py
@@ -197,9 +197,10 @@ def predict(predinfo, fitinfo, emu, x, args=None):
     emumean = emupredict.mean() + emubias.mean().T
     emucovxhalf = emupredict.covxhalf()
     varfull = np.sum(np.square(emucovxhalf), axis=2)
+    emubiascovxhalf = np.squeeze(emubias.covxhalf())
 
     for k in range(0, theta.shape[0]):
-        re = emucovxhalf[:, k, :] @ \
+        re = (emucovxhalf[:, k, :] + emubiascovxhalf.reshape(x.shape[0], 1)) @ \
             sps.norm.rvs(0, 1, size=(emucovxhalf.shape[2]))
         predinfo['rnd'][k, :] += re
 
@@ -265,6 +266,7 @@ def loglik(fitinfo, emu, emu_bias, theta, y, x, args):
     emumean = emupredict.mean() + emubias.mean().T
     emuvar = emupredict.var() + emubias.var().T
     emucovxhalf = emupredict.covxhalf()
+    emubiascovxhalf = np.squeeze(emubias.covxhalf())
     loglik = np.zeros((emumean.shape[1], 1))
 
     if np.any(np.abs(emuvar/(10 ** (-4) +
@@ -279,7 +281,7 @@ def loglik(fitinfo, emu, emu_bias, theta, y, x, args):
     # compute loglikelihood for each theta value in theta
     for k in range(0, emumean.shape[1]):
         m0 = emumean[:, k]
-        S0 = np.squeeze(emucovxhalf[:, k, :])
+        S0 = np.squeeze(emucovxhalf[:, k, :] + emubiascovxhalf.reshape(x.shape[0], 1))
         stndresid = (np.squeeze(y) - m0) / np.sqrt(obsvar)
         term1 = np.sum(stndresid ** 2)
         J = (S0.T / np.sqrt(obsvar)).T
@@ -314,6 +316,7 @@ def loglik_grad(fitinfo, emu, emu_bias, theta, y, x, args):
     emumean = emupredict.mean()
     emuvar = emupredict.var() + emubias.var().T
     emucovxhalf = emupredict.covxhalf()
+    emubiascovxhalf = np.squeeze(emubias.covxhalf())
     emumean_grad = emupredict.mean_gradtheta()
     emucovxhalf_grad = emupredict.covxhalf_gradtheta()
 
@@ -337,7 +340,7 @@ def loglik_grad(fitinfo, emu, emu_bias, theta, y, x, args):
     for k in range(0, emumean.shape[1]):
         m0 = emumean[:, k]
         dm0 = np.squeeze(emumean_grad[:, k, :])
-        S0 = np.squeeze(emucovxhalf[:, k, :])
+        S0 = np.squeeze(emucovxhalf[:, k, :] + emubiascovxhalf.reshape(x.shape[0], 1))
         stndresid = (np.squeeze(y) - m0) / np.sqrt(obsvar)
         term1 = np.sum(stndresid ** 2)
         stndresid_grad = - (dm0.T / np.sqrt(obsvar)).T